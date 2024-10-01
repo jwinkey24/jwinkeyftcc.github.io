608072396973d3b07b22d582e39dcfc10f56f2d6
jwinkey24 <winkeyj2142@student.faytechcc.edu>
2024-10-01 13:30:32 -0400

    Add files via upload
    p1lab

---
+++ P1Lab2_WinkeyJamil.py
@@ -0,0 +1,15 @@
+#Jamil Winkey
+#10/1/24
+#P1Lab2
+#Putting python code in github
+
+
+print("What is your first name?: ");
+fname = input();
+print("What is your last name?: ");
+lname = input();
+print("What is your favorite activity");
+activity =input();
+
+print(" Your name is: ",  fname + lname)
+print("Your favorite activity is: ", activity, "nice");
